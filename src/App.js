7f3beac3fc9311c0127877f4ec765fcc63ba811b
Manny Cortez Studios <emailme@mannycortez.com>
2019-09-04 16:56:13 -0400

    Created pages folder and added Home, Rooms, Single Room and Error pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
 function App() {
   return (
     <>
-    Hello from Real Estate App 
+    <Home />
+    <Rooms />
+    <SingleRoom />
+    <Error />
     </>
   );
 }
