76c9bf41ad94c429f01659c730ebe8e57b60198c
Manny Cortez Studios <emailme@mannycortez.com>
2019-09-04 18:34:03 -0400

    created Hero component for banner image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,7 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
 
 export default function Home() {
-    return (
-        <div>
-            Hello from Home page
-        </div>
-    )
+    return<Hero hero="defaultHero" />;
 }
 
