f257f12bfd483076d7028a53a657324c0b6877d8
Manny Cortez Studios <emailme@mannycortez.com>
2019-09-05 06:00:15 -0400

    Created boilerplate for featured rooms section

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -8,14 +8,16 @@ export default class FeaturedRooms extends
 Component {
     static contextType = RoomContext
     render() {
-        const { loading, featuredRooms: rooms } = this.context
+        let { loading, featuredRooms: rooms } = this.context
         rooms = rooms.map(room => {
             return <Room key={room.id} room={room} />
         })
         return (
-            <section>
-               
-            <Loading />
+            <section className="featured-rooms">
+            <Title title="featured rooms" />
+            <div className="featured-rooms-center">
+            {loading?<Loading />: rooms}
+            </div>   
             </section>
         )
     }
