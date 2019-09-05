5bf9b5102faf5b3d70aa3699bd966b01592a54ba
Manny Cortez Studios <emailme@mannycortez.com>
2019-09-05 05:10:14 -0400

    Created context file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { RoomProvider } from './context'
 
 ReactDOM.render(
-<Router>
-    <App />
-</Router>, 
+    <RoomProvider>
+      <Router>
+        <App />
+      </Router>
+    </RoomProvider>
+    , 
 document.getElementById('root')
 );
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import { Link } from 'react-router-dom'
 import Services from '../components/Services'
+import FeaturedRooms from '../components/FeaturedRooms'
 
 export default function Home() {
     return(
@@ -10,12 +11,14 @@ export default function Home() {
         <Hero hero="defaultHero">
             <Banner title="luxurious rooms" 
                     subtitle="deluxe rooms starting at $299">
-                <Link to="/rooms" className="btn-primary">
+                <Link to="/rooms" 
+                      className="btn-primary">
                     our rooms
                 </Link>
             </Banner>
         </Hero>
         <Services />
+        <FeaturedRooms />
         </>
   );
 }
